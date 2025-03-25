7a27a696d97c0543bf9485c961f57486fcb45264
kevjo-ops <kevinjonathanpilcohuillca@gmail.com>
2025-03-25 18:55:02 -0500

    Update politica.js

--- politica.js
+++ politica.js
@@ -1,27 +1,25 @@
 $(document).ready(function(){
-    // Inicializar el carrusel de certificaciones
     var owl = $("#certificaciones-carousel").owlCarousel({
         loop: true,
         margin: 30,
-        nav: false,  // Cambiado a false para ocultar los controles nativos
+        nav: false, 
         dots: true,
         autoplay: true,
         autoplayTimeout: 5000,
         autoplayHoverPause: true,
         responsive: {
             0: {
-                items: 1 // Móviles: 1 elemento
+                items: 1 
             },
             600: {
-                items: 1 // Tablets: 1 elemento
+                items: 1 
             },
             992: {
-                items: 3 // Escritorio: 3 elementos
+                items: 3 
             }
         }
     });
-    
-    // Botones de navegación personalizados
+
     $(".custom-prev").click(function(){
         owl.trigger('prev.owl.carousel');
     });
